69f39a6c6e751cf523bac876a998e4659038f42d
wangxinistudousi <14301046@bjtu.edu.cn>
2016-12-20 22:51:09 +0800

    my part

---
+++ FollowingNumberServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.edu.bjtu.weibo.service;
+
+/**
+ * Usually it will show automatically beside the user profile.
+ * 
+ * @author Liu Jinfeng
+ *
+ */
+
+public FollowingNumberServiceImpl implement FollowingNumberService{
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	int getFollowingNumber(String userId)
+	{
+		return followingNumbers£»
+	}
+}
