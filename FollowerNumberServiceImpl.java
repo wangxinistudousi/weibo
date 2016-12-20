9a23b3767a8c108bce174f0dc345da8ca6969ba0
wangxinistudousi <14301046@bjtu.edu.cn>
2016-12-20 22:46:04 +0800

    javaeee

---
+++ FollowerNumberServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.edu.bjtu.weibo.service;
+
+/**
+ * Usually it will show automatically beside the user profile.
+ * 
+ * @author Liu Jinfeng
+ *
+ */
+
+public FollowerNumberServiceImpl implement FollowerNumberService{
+	/**
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	int getFollowerNumber(String userId)
+	{
+		return followerNumbers£»
+	}
+}
